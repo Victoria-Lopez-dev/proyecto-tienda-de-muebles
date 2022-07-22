0a953050e57e8bd278dc5b1e8d1b5eb48d725e96
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2022-07-22 12:35:22 -0300

    alt-change-DB

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2')
 
 const databaseConnection = mysql.createConnection({
-  host:'bcq7wkjgujvunj972pbo-mysql.services.clever-cloud.com',
-  user:'uz6yxqqazf4h6jyo',
-  database:'bcq7wkjgujvunj972pbo',
-  password:'3d0xCh3oVJFC71E17rbN',
+  host:'localhost',
+  user:'root',
+  database:'tiendaMueble',
+  // host:'bcq7wkjgujvunj972pbo-mysql.services.clever-cloud.com',
+  // user:'uz6yxqqazf4h6jyo',
+  // database:'bcq7wkjgujvunj972pbo',
+  // password:'3d0xCh3oVJFC71E17rbN',
 })
 
 databaseConnection.connect((error)=>{
--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:false}));
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'../public')));
 
 
 
